e07851527bb8212b86b708b71718ccc72279aa00
mustaq-amd <97461845+mustaq-amd@users.noreply.github.com>
2022-03-22 23:40:02 +0530

    problem1 day13 streamapi

---
+++ Practice/src/com/masai/assignments/day13/problem1/Main.java
@@ -0,0 +1,24 @@
+package com.masai.assignments.day13.problem1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Student> list=new ArrayList<>();
+
+        list.add(new Student(23,"Chandu",950));
+        list.add(new Student(13,"Maheswar",650));
+        list.add(new Student(29,"Sandeep",750));
+        list.add(new Student(12,"Akhil",700));
+        list.add(new Student(56,"Midhun",850));
+
+        Stream<Student> students= list.stream().filter(student -> student.getMarks()<800);
+        students.forEach(System.out::println);
+
+
+    }
+}
---
+++ Practice/src/com/masai/assignments/day13/problem1/Student.java
@@ -0,0 +1,50 @@
+package com.masai.assignments.day13.problem1;
+
+public class Student {
+
+    private int rollNumber;
+    private String studentName;
+    private int marks;
+
+    public Student() {
+    }
+
+    public Student(int rollNumber, String studentName, int marks) {
+        this.rollNumber = rollNumber;
+        this.studentName = studentName;
+        this.marks = marks;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "rollNumber=" + rollNumber +
+                ", studentName='" + studentName + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+}
