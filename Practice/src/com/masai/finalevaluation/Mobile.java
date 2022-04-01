7eae6295d2fa22586d34a185b9a4467968e5d3d3
mustaq-amd <97461845+mustaq-amd@users.noreply.github.com>
2022-04-01 14:26:06 +0530

    added

--- Practice/src/com/masai/finalevaluation/Mobile.java
+++ Practice/src/com/masai/finalevaluation/Mobile.java
@@ -7,6 +7,7 @@ public class Mobile {
 
     HashMap<String, ArrayList<String>> mobiles = new HashMap<>();
 
+
     public String addMobile(String company, String model) {
 
         String message="Mobile not added";
@@ -14,30 +15,30 @@ public class Mobile {
         Set<Map.Entry<String, ArrayList<String>>> set=mobiles.entrySet();
         List <Map.Entry<String, ArrayList<String>>> list=new ArrayList<>(set);
 
-        ArrayList<String> models=new ArrayList<>();
-        models.add(model);
-
-
         boolean result=list.stream().anyMatch(e-> {
             return e.getKey()==company;
         });
 
+        ArrayList<String> models=new ArrayList<>();
+        models.add(model);
+
         if(result){
-            models.add(model);
-            mobiles.put(company,models);
+            ArrayList<String> sameCompany= getModels(company);
+            sameCompany.add(model);
+            mobiles.put(company,sameCompany);
             message="Mobile Added Successfully";
 
         }else{
             mobiles.put(company,models);
             message="Mobile Added Successfully";
         }
-        System.out.println(mobiles);
 
 
         return  message;
     }
 
     public ArrayList<String> getModels(String company){
+
         ArrayList<String> list=null;
 
         list=mobiles.get(company);
@@ -51,10 +52,17 @@ public class Mobile {
 
         Mobile mobile=new Mobile();
         System.out.println(mobile.addMobile("Apple","Iphone13"));
+        System.out.println(mobile.addMobile("Samsung","S11"));
+        System.out.println(mobile.addMobile("Apple","Iphone12"));
+        System.out.println(mobile.addMobile("Samsung","S10"));
+        System.out.println(mobile.addMobile("Apple","IphoneXR"));
 
 
         List<String> models=mobile.getModels("Apple");
         System.out.println(models);
+
+        List<String> models1=mobile.getModels("Samsung");
+        System.out.println(models1);
 
 
     }
