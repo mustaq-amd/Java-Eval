7c5bf6b628870a322b8a7d4e4f423fead23b4c71
Mohammed Mustaq <97461845+mustaq-amd@users.noreply.github.com>
2022-04-25 13:26:21 +0530

    Add files via upload

---
+++ unit5C3/Q3/firstSpringProject/src/main/java/com/masai/model/Rectangle.java
@@ -0,0 +1,12 @@
+package com.masai.model;
+
+public class Rectangle {
+	
+public double calculateArea(int length,int breadth) {
+		
+		double area=length*breadth;
+		
+		return area;
+	}
+
+}
---
+++ unit5C3/Q3/firstSpringProject/src/main/java/com/masai/model/Square.java
@@ -0,0 +1,12 @@
+package com.masai.model;
+
+public class Square {
+	
+	public double calculateArea(int side) {
+		
+		double area=side*side;
+		
+		return area;
+	}
+
+}
